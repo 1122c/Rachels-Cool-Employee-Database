3fae4b0f4df336dfa96680171a68c53674b1c85e
1122c <118850768+1122c@users.noreply.github.com>
2024-02-15 11:33:39 -0800

    inquirer questions to run query functions

--- server.js
+++ server.js
@@ -1,2 +1,60 @@
-const connection = require("./connection/db.js")
-const inquirer = require("inquirer")
+const connection = require("./connection/db.js");
+const inquirer = require("inquirer");
+
+function init() {
+  inquirer
+    .prompt([
+      {
+        name: "option",
+        type: "list",
+        message: "What action would you like to take?",
+        choices: [
+          "View all departments",
+          "View all roles",
+          "View all employees",
+          "Add a new department",
+          "Add a new role",
+          "Add an employee",
+          "Update employee roles",
+          "Exit",
+        ],
+      },
+    ])
+    .then(function (response) {
+      switch (response.option) {
+        case "View all departments":
+          viewDepartments();
+          break;
+        case "View all roles":
+          viewRoles();
+          break;
+        case "View all employees":
+          viewEmployees();
+          break;
+        case "Add a new department":
+          addDepartment();
+          break;
+        case "Add a new role":
+          addRole();
+          break;
+        case "Add an employee":
+          addEmployee();
+          break;
+        case "Update employee roles":
+          updateEmployee();
+          break;
+        case "exit":
+          connection.end();
+          break;
+      }
+    });
+}
+
+
+
+
+
+
+
+
+init()
